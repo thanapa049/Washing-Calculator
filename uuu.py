b989e58ad1a433c2b452021c4b24a85d5655b0c0
thanapa049 <s7070049@kmitl.ac.th>
2014-12-08 21:54:01 +0700

    แก้ program
    แก้ or เป็น and

--- uuu.py
+++ uuu.py
@@ -25,14 +25,14 @@ def washingmachine():
         if light > 0:
             nextround += 1
         print washround, nextround
-    elif dark > 0 or light == 0:
+    elif dark > 0 and light == 0:
         while dark > (weight_machine/2):
             washround += 1
             dark -= weight_machine
         if dark > 0:
             nextround += 1
         print washround, nextround
-    elif light > 0 or dark == 0:
+    elif light > 0 and dark == 0:
         while light > weight_machine:
             washround += 1
             light -= weight_machine
